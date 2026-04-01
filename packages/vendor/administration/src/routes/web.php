a312160279a1f64229d150f2fcb80d59c76f5333
wahid fkiri <wahidfkiri5@gmail.com>
2026-04-01 17:02:14 +0200

    make max upload 100M

--- packages/vendor/administration/src/routes/web.php
+++ packages/vendor/administration/src/routes/web.php
@@ -26,7 +26,7 @@ Route::prefix('sliders')->group(function () {
     Route::delete('/{id}/force-delete', [SliderController::class, 'forceDelete'])->name('sliders.force-delete');
     Route::post('/{id}/toggle-status', [SliderController::class, 'toggleStatus'])->name('sliders.toggle-status');
     Route::post('/update-order', [SliderController::class, 'updateOrder'])->name('sliders.update-order');
-    Route::get('/statistics', [SliderController::class, 'statistics'])->name('sliders.statistics');
+    Route::get('/statistics/data', [SliderController::class, 'statistics'])->name('sliders.statistics');
     Route::get('/{id}/preview', [SliderController::class, 'preview'])->name('sliders.preview');
 });
 
